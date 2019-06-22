cb8c4b740173573d4d18106c31d978944007a686
國分貞史 <48110743+Namerou7716@users.noreply.github.com>
2019-06-23 08:00:28 +0900

    OmochaLaboCamera

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.InteropServices;
+
+public class ScreenShot : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void SavaToAlbum(string path);
+    IEnumerator SaveToCameraroll(string path)
+    {
+        while (true)
+        {
+            if (File.Exists(path)) break;
+
+            yield return null;
+        }
+        SavaToAlbum(path);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+#if UNITY_EDITOR
+#else
+            string filename="test.png";
+            string path = Application.persistentDataPath+"/"+filename;
+
+            File.Delete(path);
+
+            ScreenCapture.CaptureScreenshot(filename);
+            StartCoroutine(SaveToCameraroll(path));
+#endif
+        }
+    }
+}
+
