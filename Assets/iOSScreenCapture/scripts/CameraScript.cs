04fa8d45d30a1ef9787e5b59c09a3489e8af4efa
國分貞史 <48110743+Namerou7716@users.noreply.github.com>
2019-06-23 03:27:27 +0900

    Add CameraScript and CaptureScript

---
+++ Assets/iOSScreenCapture/scripts/CameraScript.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class CameraScript : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void _PlaySystemShutterSound();
+
+    [DllImport("__Internal")]
+    private static extern void _SendTexture(byte[] textureByte, int length);
+
+    [DllImport("__Internal")]
+    private static extern void _RequestCameraPermission();
+
+    [DllImport("__Internal")]
+    private static extern void _RequestCameraRollPermission();
+
+    [DllImport("__Internal")]
+    private static extern int _HasCameraPermission();
+
+    [DllImport("__Internal")]
+    private static extern int _HasCameraRollPermission();
+
+    [DllImport("__Internal")]
+    private static extern void _GoToSettings();
+
+    public static void PlaySystemShutterSound()
+    {
+        _PlaySystemShutterSound();
+    }
+
+    public static void SaveTexture(byte[] textureByte, int length)
+    {
+        _SendTexture(textureByte, length);
+    }
+    public static void RequestPermissions()
+    {
+        AVAuthorizationStatus avstatus = HasCameraPermission();
+        PHAuthorizationStatus phstatus = HasCameraRollPermission();
+        if (avstatus == AVAuthorizationStatus.NotDetermined)
+        {
+            _RequestCameraPermission();
+        }
+        if (phstatus == PHAuthorizationStatus.NotDetermined)
+        {
+            _RequestCameraRollPermission();
+        }
+    }
+    //adapt Button
+    public void RequestPermissions_forGUI()
+    {
+        RequestPermissions();
+    }
+    public static AVAuthorizationStatus HasCameraPermission()
+    {
+#if !UNITY_EDITOR
+        return(AVAuthorizationStatus)Enum.ToObject(
+                                    typeof(AVAuthorizationS),_HasCameraPermission());
+#endif
+        return AVAuthorizationStatus.Authorized;
+
+    }
+    public static PHAuthorizationStatus HasCameraRollPermission()
+    {
+#if !UNITY_EDITOR
+        return (PHAuthorizationStatus)Enum.ToObject(
+            typeof(PHAuthorizationStatus), HasCameraRollPermission());
+#endif
+        return PHAuthorizationStatus.Authorized;
+    }
+
+    public static void GoTOSetting()
+    {
+#if !UNITY_EDITOR
+        _GoToSettings();
+#endif
+    }
+
+    public void GoToSetting_forGUI()
+    {
+#if !UNITY_EDITOR
+        _GoToSettings();
+#endif
+    }
+}
---
+++ Assets/iOSScreenCapture/scripts/CaptureScript.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CaptureScript : MonoBehaviour
+{
+    public UnityEvent OnCompleteCapture;
+    public UnityEvent OnFailCapture;
+
+    public void Execute()
+    {
+#if UNITY_EDITORx
+        PHAuthorizationStatus phstatus = (PHAuthorizationStatus)Enum.ToObject(
+            typeof(PHAuthorizationStatus), CameraScript.HasCameraRollPermission());
+        CameraScript.PlaySystemShutterSound();
+        if (phstatus == PHAuthorizationStatus.Authorized)
+        {
+            Handheld.SetActivityIndicatorStyle(UnityEngine.iOS.ActivityIndicatorStyle.Gray);
+            Handheld.StartActivityIndicator();
+            StartCoroutine(_CapturesScreenShot());
+        }
+        else
+        {
+            OnFailCapture.Invoke();
+        }
+#endif
+    }
+
+    private IEnumerator _CaptureScreenShot()
+    {
+        //canvasGroup.alpha=0;
+        yield return new WaitForEndOfFrame();
+        var width = Screen.width;
+        var height = Screen.height;
+        var tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+        byte[] screenshot = tex.EncodeToPNG();
+
+        CameraScript.SaveTexture(screenshot, screenshot.Length);
+        //canvasGroup.alpha=1;
+    }
+
+    void DidImageWriteToAlbum(string errorDescription)
+    {
+        Handheld.StopActivityIndicator();
+        if (string.IsNullOrEmpty(errorDescription))
+        {
+            OnCompleteCapture.Invoke();
+        }
+        else
+        {
+            OnFailCapture.Invoke();
+        }
+    }
+}
