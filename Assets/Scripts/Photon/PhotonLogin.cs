0a0ec5bd680689693b26832e629d2d107c1dc108
Achu0810 <achu.blog0810@gmail.com>
2019-06-23 11:58:17 +0900

    no message

---
+++ Assets/Scripts/Photon/PhotonLogin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class PhotonLogin : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] private GameObject player;
+    private AlphaChanging alphaChanging;
+/* 
+    void Start()
+    {
+        alphaChanging = GameObject.FindObjectOfType<AlphaChanging>();
+        if(alphaChanging.GetIsCreatingRoom()) {
+            PhotonNetwork.Instantiate(player.name, Vector3.zero, Quaternion.identity, 0);
+        }
+    }
+    */
+}
+
--- Assets/Scripts/StartScene/AlphaChanging.cs
+++ Assets/Scripts/StartScene/AlphaChanging.cs
@@ -1,18 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
-public class AlphaChanging : MonoBehaviour
+public class AlphaChanging : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private float changingSpeed;
+    [SerializeField] private GameObject selectButtons;
+    [SerializeField] private GameObject inputFields;
+    [SerializeField] private UnityEngine.UI.Text roomNameText;
+    [SerializeField] private GameObject player;
+    private UnityEngine.UI.Image logoImage;
+    private string roomName;
+    private bool isCreatingRoom;
+
     void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+        logoImage = GetComponent<UnityEngine.UI.Image>();
+        StartCoroutine(Splash());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    IEnumerator Splash() {
+        float alpha = 0;
+        while(logoImage.color.a < 1.0f) {
+            alpha += Time.deltaTime * changingSpeed;
+            logoImage.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1.0f);
+        while(logoImage.color.a > 0) {
+            alpha -= Time.deltaTime * changingSpeed;
+            logoImage.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.75f);
+        selectButtons.SetActive(true);
+    }
+
+    public void OnClickedCreateRoom() {
+        selectButtons.SetActive(false);
+        inputFields.SetActive(true);
+        isCreatingRoom = true;
+    }
+
+    public void OnClickedJoinRoom() {
+        selectButtons.SetActive(false);
+        inputFields.SetActive(true);
+        isCreatingRoom = false;
+    }
+
+    public void OnClickedReturnButton() {
+        inputFields.SetActive(false);
+        selectButtons.SetActive(true);
+    }
+
+    public void OnClickedOKButton() {
+        roomName = roomNameText.text;
+        inputFields.SetActive(false);
         
+        // つくる側
+        if(isCreatingRoom) {
+            PhotonNetwork.ConnectUsingSettings();
+        // 参加する側
+        } else {
+
+        }
+    }
+
+    // complated connecting master server
+    public override void OnConnectedToMaster() {
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions(), TypedLobby.Default);
+    }
+
+    // completed connecting room
+    public override void OnJoinedRoom() {
+        SceneManager.LoadScene("Editor");
+    }
+
+    public bool GetIsCreatingRoom() {
+        return isCreatingRoom;
     }
 }
